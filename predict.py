fb1c29008dbbf6c1be67ea1d93958f97140a6468
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-11-30 11:30:26 +0300

    ready

--- predict.py
+++ predict.py
@@ -18,7 +18,7 @@ class Predictor(BasePredictor):
         running multiple predictions efficient"""
         print('-------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
         self.pipeline = AutoPipelineForInpainting.from_pretrained(
-            "./Realistic_Vision_V3.0_VAE",
+            "./epicrealism_pureevolutionv5-inpainting",
             torch_dtype=torch.float16,
             variant="fp16",
             requires_safety_checker=False,
