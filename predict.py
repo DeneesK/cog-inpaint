61086c05cf7658dcdb8c6d3f7b5e45ffbc1f9016
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-07 11:31:39 +0300

    ready

--- predict.py
+++ predict.py
@@ -5,8 +5,9 @@ import random
 sys.path.insert(0, "stylegan-encoder")
 import tempfile  # noqa
 from cog import BasePredictor, Input, Path  # noqa
-from diffusers import StableDiffusionInpaintPipeline, StableDiffusionPipeline  # noqa
+from diffusers import StableDiffusionControlNetInpaintPipeline, ControlNetModel, UniPCMultistepScheduler  # noqa
 import torch  # noqa
+import numpy as np
 
 from PIL import Image  # noqa
 from diffusers.utils import load_image  # noqa
@@ -20,16 +21,30 @@ def disabled_safety_checker(images, clip_input):
         return images, False
 
 
+def make_inpaint_condition(image, image_mask):
+    image = np.array(image.convert("RGB")).astype(np.float32) / 255.0
+    image_mask = np.array(image_mask.convert("L")).astype(np.float32) / 255.0
+
+    assert image.shape[0:1] == image_mask.shape[0:1], "image and image_mask must have the same image size"
+    image[image_mask > 0.5] = -1.0  # set as masked pixel
+    image = np.expand_dims(image, 0).transpose(0, 3, 1, 2)
+    image = torch.from_numpy(image)
+    return image
+
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make
         running multiple predictions efficient"""
         print('-------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
-        self.pipeline = StableDiffusionInpaintPipeline.from_single_file(
+        self.controlnet = ControlNetModel.from_pretrained("./control_v11p_sd15_openpose", use_safetensors=True)
+        self.pipeline = StableDiffusionControlNetInpaintPipeline.from_single_file(
             "./epiCRealism/epicrealism_v10-inpainting.safetensors",
+            controlnet=self.controlnet,
             requires_safety_checker=False,
             use_safetensors=True
         ).to("cuda")
+        self.pipeline.scheduler = UniPCMultistepScheduler.from_config(self.pipeline.scheduler.config)
         self.pipeline.enable_model_cpu_offload()
         print('-------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
@@ -69,7 +84,6 @@ class Predictor(BasePredictor):
 
             if not seed:
                 seed = random.randint(0, 99999)
-
             init_image = load_image(str(image))
             w, h = resize_(init_image)
             init_image.resize((w, h))
@@ -79,6 +93,7 @@ class Predictor(BasePredictor):
             torch.cuda.empty_cache()
             print('-------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
             self.pipeline.safety_checker = disabled_safety_checker
+            control_image = make_inpaint_condition(init_image, mask_image)
             image = self.pipeline(prompt=prompt,
                                   negative_prompt=negative_prompt,
                                   image=init_image,
@@ -88,7 +103,8 @@ class Predictor(BasePredictor):
                                   guidance_scale=int(guidance_scale),
                                   strength=strength,
                                   width=w,
-                                  height=h
+                                  height=h,
+                                  control_image=control_image,
                                   ).images[0]
             print(image)
             image.save(out_path)
