b978c03450caf6db70719d177051fcff5eee7e94
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-13 17:35:19 +0300

    ready

--- predict.py
+++ predict.py
@@ -43,8 +43,8 @@ class Predictor(BasePredictor):
             torch_dtype=torch.float16
             )
         self.pipeline: StableDiffusionControlNetInpaintPipeline = \
-            StableDiffusionControlNetInpaintPipeline.from_single_file(
-                "https://huggingface.co/patsanchique/uberRealisticPornMerge_urpmv13Inpainting/blob/main/uberRealisticPornMerge_urpmv13Inpainting.safetensors",
+            StableDiffusionControlNetInpaintPipeline.from_pretrained(
+                "./epicrealism_pureevolutionv5-inpainting",
                 use_safetensors=True,
                 torch_dtype=torch.float16,
                 requires_safety_checker=False,
